3ef209a7c39f99e889c4b49880f06e4636db37ea
MuhammadDikaisa <115516378+MuhammadDikaisa@users.noreply.github.com>
2022-11-07 14:06:14 +0700

    Add files via upload

---
+++ Kondisionaldanperulangan/Latihan1.py
@@ -0,0 +1,7 @@
+bil1 = int(input("Masukkan Bilangan Ke-1 : "))
+bil2 = int(input("Masukkan Bilangan Ke-2 : "))
+
+if(bil1 > bil2) :
+    print("Bilangan ", bil1, "Lebih Besar Dari Bilangan ", bil2)
+else :
+    print("Bilangan ", bil2, "Lebih Besar Dari Bilangan ", bil1)
