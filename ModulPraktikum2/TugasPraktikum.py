d3e7d0966039ac6c9a8ff4c45ad5728f09a64341
MuhammadDikaisa <115516378+MuhammadDikaisa@users.noreply.github.com>
2022-11-07 14:11:44 +0700

    Add files via upload

--- ModulPraktikum2/TugasPraktikum.py
+++ ModulPraktikum2/TugasPraktikum.py
@@ -1,1 +1,10 @@
+a = int(input("Bilangan Ke-1 : "))
+b = int(input("Bilangan Ke-2 : "))
+c = int(input("Bilangan Ke-3 : "))
 
+if a>b and a>c :
+    print(a)
+elif b>a and b>c :
+    print(b)
+else :
+    print(c)
