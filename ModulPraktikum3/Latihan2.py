30160892ce0a0015ea4ace0d5408ed5eb125b09c
MuhammadDikaisa <115516378+MuhammadDikaisa@users.noreply.github.com>
2022-11-07 14:12:06 +0700

    Add files via upload

--- ModulPraktikum3/Latihan2.py
+++ ModulPraktikum3/Latihan2.py
@@ -3,4 +3,4 @@ import random
 jumlah = int(input("Masukkan Jumlah N : "))
 
 for i in range (jumlah) :
-    print("Data ke-", i+1, " adalah ", (random.uniform(0.1, 0.5))) 
+    print("Data ke-", i+1, " adalah ", (random.uniform(0.1, 0.5)))
